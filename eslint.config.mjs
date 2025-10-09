3099635b1e220ececaafeeec4db727244e8ac6b1
nieukki <tomekpacek@o2.pl>
2025-10-09 10:22:00 +0200

    eslint cfg

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -18,7 +18,17 @@ const eslintConfig = [
       "out/**",
       "build/**",
       "next-env.d.ts",
+      "src/generated/**",
+      "prisma/generated/**",
     ],
+  },
+  {
+    extends: "next/core-web-vitals",
+    rules: {
+      "@typescript-eslint/no-unused-expressions": "off",
+      "@typescript-eslint/no-unused-vars": "warn",
+      "@typescript-eslint/no-require-imports": "off",
+    },
   },
 ];
 
